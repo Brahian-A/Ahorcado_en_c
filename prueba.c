969f19bb2aecb9810aebe52290ff32a5524f0482
Brahia-A <10114@holbertonstudents.com>
2025-07-13 15:55:19 -0300

    mejoras

--- prueba.c
+++ prueba.c
@@ -5,6 +5,9 @@
 #include <time.h>
 #include <ctype.h>
 
+#define MAX_PALABRAS 100
+#define MAX_LONGITUD 50
+
 void mostrar_barra_vida(int errores, int max_errores)
 {
     int llenos = max_errores - errores;
@@ -46,6 +49,22 @@ int main(void)
         fclose(archivo_victorias);
     }
 
+    // Leer palabras desde archivo
+    char palabras[MAX_PALABRAS][MAX_LONGITUD];
+    int cantidad_palabras = 0;
+
+    FILE *archivo_palabras = fopen("palabras.txt", "r");
+    if (archivo_palabras == NULL) {
+        perror("No se pudo abrir palabras.txt");
+        return 1;
+    }
+
+    while (fgets(palabras[cantidad_palabras], MAX_LONGITUD, archivo_palabras) != NULL && cantidad_palabras < MAX_PALABRAS) {
+        palabras[cantidad_palabras][strcspn(palabras[cantidad_palabras], "\n")] = '\0';
+        cantidad_palabras++;
+    }
+    fclose(archivo_palabras);
+
     char respuesta[10];
     char letras_falladas[30] = "";
     int cantidad_falladas = 0;
@@ -60,22 +79,17 @@ int main(void)
         cantidad_falladas = 0;
         const int max_errores = 6;
         char solucion[50];
-        const char *words[] = {
-            "programacion", "computadora", "teclado", "raton", "pantalla",
-            "lenguaje", "desarrollador", "variable", "funcion", "compilador",
-            "algoritmo", "memoria", "procesador", "software", "hardware",
-            "internet", "servidor", "base", "datos", "codigo",
-            "archivo", "carpeta", "consola", "clic", "red",
-            "usb", "aplicacion", "sistema", "usuario", "clave",
-            "nube", "sitio", "web", "correo", "imagen",
-            "video", "enlace", "programa", "navegador", "router",
-            "wifi", "control", "barra", "boton", "ventana",
-            "icono", "descarga", "registro", "monitor", "celular",
-            "tablet", "login", "logout", "pantallazo", "atajo",
-            "actualizar", "formato", "buscador", "navegar", "scroll",
-            "copiar", "pegar", "reiniciar", "bateria", "subir",
-            "mensaje"
-        };
+        char palabra_oculta[50];
+
+        srand(time(NULL));
+        int index = rand() % cantidad_palabras;
+        strcpy(palabra_oculta, palabras[index]);
+        len_word = strlen(palabra_oculta);
+
+        for (int i = 0; i < len_word; i++)
+            solucion[i] = '_';
+        solucion[len_word] = '\0';
+
         char matriz[5][6] = {
             "     ",
             "     ",
@@ -83,16 +97,6 @@ int main(void)
             "     ",
             "     "
         };
-
-        srand(time(NULL));
-        int index = rand() % 64;
-        char palabra_oculta[50];
-        strcpy(palabra_oculta, words[index]);
-        len_word = strlen(palabra_oculta);
-
-        for (int i = 0; i < len_word; i++)
-            solucion[i] = '_';
-        solucion[len_word] = '\0';
 
         while (69)
         {
@@ -133,7 +137,6 @@ int main(void)
                     letras_falladas[cantidad_falladas++] = letra;
                     letras_falladas[cantidad_falladas] = '\0';
                 }
-
                 errores++;
                 actualizar_matriz(matriz, errores);
             }
@@ -149,7 +152,7 @@ int main(void)
                     printf("Letras falladas: %s\n", letras_falladas);
                 printf("¡Ganaste! La palabra era: %s\n", palabra_oculta);
                 victorias++;
-                printf("%d %s\n", victorias, victorias == 1 ? "Victoria" : "Victorias");
+                printf(victorias == 1 ? "1 Victoria\n" : "%d Victorias\n", victorias);
                 sleep(2);
                 break;
             }
